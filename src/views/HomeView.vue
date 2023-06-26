f28d1f88e3e2d07ccff07907b5e515fee679f354
TRAN THI VAN ANH <54926540+vananh1993@users.noreply.github.com>
2023-06-26 08:20:09 +0700

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import axios from 'axios';
+import TheWelcome from '../components/TheWelcome.vue';
+
+let config = {
+  headers: {
+    'app-id': import.meta.env.VITE_APP_DUMMY_API_KEY,
+  }
+};
+
+console.log(import.meta.env.VITE_APP_DUMMY_API_KEY)
+
+axios.get('https://dummyapi.io/data/v1/post?created=1', config).then(console.log);
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
