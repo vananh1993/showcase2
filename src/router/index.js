dde18743b48a7522f0e49bdcda234e43d2e22584
vananh1993 <vananh.tran@thinkclimax.com>
2023-06-28 18:00:07 +0700

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('@/views/ShowcaseView.vue')
+    },
+    {
+      path: '/list/:category',
+      name: 'listFromCate',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('@/views/ShowcaseFromCateView.vue')
     }
   ]
 })
---
+++ src/views/ShowcaseFromCateView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">{{ title }}</h1>
+    <div class="row">
+        <div class="col-sm-4 mt-4" v-for="showcases in getShowcases" :key="showcases.id">
+            <div class="">
+                <img src="../assets/thumb-showcase/350x200.png" alt="">
+                <p>{{ showcases.title }}</p>
+            </div>
+        </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+    import { ref, onMounted, computed } from "vue";
+    import { useShowcaseStore } from "../stores/showcases";
+    const store = useShowcaseStore();
+    const title = ref("List Showcase");
+    const getShowcases = computed(() => {
+      return store.getShowcases;
+    });
+    // const showcasess = computed(() => {
+    //   return store.showcasess;
+    // });
+    onMounted(() => {
+      store.fetchShowcases();
+    });
+</script>
+<style lang="scss" scoped media="screen">
+
+</style>
