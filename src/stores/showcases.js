91fb861365b48d8eb8270b2a37634d8887b19a1d
Thanh Dao <duythanh-dao@am-bition.vn>
2023-07-08 11:30:09 +0700

    - Search & pagination

--- src/stores/showcases.js
+++ src/stores/showcases.js
@@ -23,6 +23,30 @@ export const useShowcaseStore = defineStore("showcase", {
                 // alert(error)
                 console.log(error)
             }
+        },
+
+        getItemsByPage(page, search = null, limit = 12) {
+            return new Promise((resolve) => setTimeout(
+                () => {
+                    if (!search) {
+                        return resolve(this.showcases.slice(Math.max(0, page - 1) * limit, page * limit));
+                    }
+
+                    search = search.toLowerCase().trim();
+                    const searchParts = search.split(' ');
+
+                    resolve(this.showcases
+                        .map((item) => ({
+                            ...item,
+                            keywords: item.title.toLowerCase().split(' ')
+                        }))
+                        .filter((item) => {
+                            return item.title.toLowerCase().includes(search)
+                                || item.keywords.some((keyword) => searchParts.includes(keyword));
+                        })
+                        .slice(Math.max(0, page - 1) * limit, page * limit));
+                }, 500
+            ));
         }
     },
 })
--- src/views/ShowcaseView.vue
+++ src/views/ShowcaseView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="container mx-auto px-4">
+  <div class="container mx-auto px-4 pt-10 pb-20">
+    <input
+        type="text"
+        v-model="search"
+        class="border border-gray-600 p-4"
+        @keyup.enter="searchShowcases"
+    />
     <h1 class=" py-7 text-center text-3xl font-semibold uppercase text-gray-800">{{ title }}</h1>
     <div class="grid grid-cols-4 gap-6 list-showcase">
         <div v-for="showcase in showcases" :key="showcase.id" class=" list-showcase__item max-w-xs rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer overflow-hidden">
@@ -8,7 +14,7 @@
                     <img :src="'../src/assets/thumb-showcase/' + showcase.imgUrl" alt="">
                 </div>
                 <div class="py-3 px-4 bg-white">
-                    <h3 class="text-lg font-semibold text-gray-600">{{ showcase.title }}</h3>
+                    <h3 class="text-lg font-semibold text-gray-600">{{ showcase.title }}-{{showcase.id}}</h3>
                 </div>
             </RouterLink>
             <ul class="pb-6 px-4">
@@ -18,7 +24,10 @@
             </ul>
         </div>
 
+        <!-- .... -->
     </div>
+
+    <Observer v-if="enabled" @intersect="getItems" />
 
   </div>
 </template>
@@ -26,17 +35,41 @@
 <script setup>
     import { RouterLink } from "vue-router"
     import { ref, onMounted, computed } from "vue";
+    import Observer from '@/components/IntersectionObserver.vue';
     import { useShowcaseStore } from "../stores/showcases";
     const store = useShowcaseStore();
-    const title = ref("List Showcase");
-    const showcases = computed(() => store.getShowcases);
+    
+    const limit = 12;
 
-    // const showcasess = computed(() => {
-    //   return store.showcasess;
-    // });
-    onMounted(() => {
-      store.fetchShowcases();
+    const title = ref("List Showcase");
+    const page = ref(0);
+    const enabled = ref(false);
+    const search = ref();
+    const showcases = ref([]);
+
+    onMounted(async () => {
+      await store.fetchShowcases();
+
+      enabled.value = true;
     });
+
+    const searchShowcases = async () => {
+        enabled.value = false;
+        showcases.value = [];
+        page.value = 0;
+
+        await getItems();
+
+        enabled.value = true;
+    };
+
+    const getItems = async () => {
+        page.value++;
+        showcases.value = [
+            ...showcases.value,
+            ...(await store.getItemsByPage(page.value, search.value, limit))
+        ];
+    }
 </script>
 <style lang="scss" scoped media="screen">
     .list-showcase {
