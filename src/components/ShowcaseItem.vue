a65ec87ddf59d041be61d6cea1946037d3260d99
vananh1993 <vananh.tran@thinkclimax.com>
2023-08-03 15:15:47 +0700

    Update ShowcaseItem.vue

--- src/components/ShowcaseItem.vue
+++ src/components/ShowcaseItem.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class=" list-showcase__item  rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer overflow-hidden">
+    <div class=" list-showcase__item rounded-t overflow-hidden border-b-2 border-gray-200 hover:border-gray-400  transition duration-500 cursor-pointer overflow-hidden">
         <!-- <RouterLink  target="_blank"> -->
             <div class="" @click.prevent="emits('selected', showcase)">
                 <div>
-                    <img :src="props.showcase.imgUrl" alt="">
+                    <img :src="props.showcase.imgUrl" alt="" class="transition duration-500 hover:scale-105">
                 </div>
-                <div class="py-3 px-4 bg-white">
+                <div class="py-3 px-0">
                     <h3 class="text-lg font-semibold text-gray-600">{{ props.showcase.title }}-{{props.showcase.id}}</h3>
                 </div>
             </div>
         <!-- </RouterLink> -->
-        <ul class="pb-6 px-4">
+        <ul class="pb-6 px-0">
             <li class="inline-block bg-green-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300 hover:bg-green-900 mt-2" v-for="tag in props.showcase.tags">
                 <a href="#/">{{ tag }}</a>
             </li>
