a8b3db4195b376fe92280e1af98d321a0e37b9e4
vananh1993 <vananh.tran@thinkclimax.com>
2023-11-17 17:14:40 +0700

    no message

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -10,6 +10,7 @@
 				<label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
 				<input type="password" v-model="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 			</p>
+			<p class="mb-10 red">{{ errMsg }}</p>
 			<p>
 				<button @click="register" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button> &nbsp;
 				<button @click="signInWithGoogle" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign In With Google</button>
@@ -26,10 +27,29 @@
 	const router = useRouter()
 	const email = ref("");
 	const password = ref("");
+	const errMsg = ref('');
+
 	const register = () => {
+		const address = email.value.split('@').pop();
+		console.log(address);
+		if (address != 'dvinci.com.vn') {
+			errMsg.value = "You don't have permissions"
+			return;
+		}
 		createUserWithEmailAndPassword(getAuth(), email.value, password.value).then((data) => {
 			// console.log('test');
-			route.push('/');
+			router.push('/');
+		})
+		.catch((error) => {
+			console.log(error.code);
+			switch (error.code) {
+				case "auth/weak-password":
+					errMsg.value = "Weak password"
+					break;
+				case "auth/email-already-in-use":
+					errMsg.value = "Email already"
+					break;
+			}
 		})
 	};
 	const signInWithGoogle = () => {
